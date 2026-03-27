fee28ce1a6fdce77de46321dacdc88a6dae8e226
Harshit yadav <harshityadav5499@gmail.com>
2026-03-27 18:33:39 +0530

    MVP : navbar including responsive design

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,14 @@
-import Navbar from "../../components/Navbar/Navbar";
-
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="w-full flex flex-col justify-center items-center bg-gray-900">
+      <div className="text-white h-screen tracking-widest flex flex-col justify-center items-center   ">
+        <div className="text-center pb-10">
+          <h1 className="text-6xl pb-4">Generate the Testimonals </h1>
+          <h1 className="text-5xl">And use them anywhere </h1>
+        </div>
+        // caousal :TODO
+         
+      </div>
+    </div>
+  );
 }
