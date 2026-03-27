9e0bdcbc37862caaef71eb185d77b6315ed66e43
Harshit yadav <harshityadav5499@gmail.com>
2026-03-27 21:19:20 +0530

    Carousal MVP done

--- src/components/LandingPage/SampleCarousal.tsx
+++ src/components/LandingPage/SampleCarousal.tsx
@@ -1,41 +1,27 @@
 "use client";
-import { useState } from "react";
+import { Images } from "@/config/ImageList";
+import { useEffect, useState } from "react";
 
-const Images = [
-  {
-    src: "image1",
-    alt: "image1",
-  },
-  {
-    src: "image2",
-    alt: "image2",
-  },
-  {
-    src: "image3",
-    alt: "image3",
-  },
-  {
-    src: "image4",
-    alt: "image4",
-  },
-  {
-    src: "image5",
-    alt: "image5",
-  },
-];
 export default function SampleCarousal() {
   const [currIndex, setCurrIndex] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrIndex((prev) => (prev + 1) % Images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className="">
-      <div className="flex items-center justify-center h-70 md:w-150 md:h-90 bg-black rounded-4xl border border-white/20">
+      <div className="flex items-center justify-center h-70 w-100 md:w-150 md:h-90 bg-black rounded-4xl border border-white/20">
         <img src={Images[currIndex].src} alt={Images[currIndex].alt} />;
       </div>
+
       <div className="flex gap-2 justify-center pt-2">
         <button
           onClick={() => {
-            console.log(currIndex);
             currIndex == 0
-              ? setCurrIndex(Images.length-1)
+              ? setCurrIndex(Images.length - 1)
               : setCurrIndex((prev) => prev - 1);
           }}
           className="bg-red-500"
@@ -44,8 +30,7 @@ export default function SampleCarousal() {
         </button>
         <button
           onClick={() => {
-            console.log(currIndex);
-            currIndex == Images.length-1
+            currIndex == Images.length - 1
               ? setCurrIndex(0)
               : setCurrIndex((prev) => prev + 1);
           }}
---
+++ src/types/images.ts
@@ -0,0 +1,5 @@
+export interface imageStructure{
+    src : string ,
+    alt : string ,
+    title? : string
+}
